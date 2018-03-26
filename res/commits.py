92381b018bf391142fc4a02c2361fbed689876d2
Pranav <pranav@shoptimize.in>
2018-03-26 09:43:36 +0530

    finished getting commits since

--- res/commits.py
+++ res/commits.py
@@ -1,18 +1,31 @@
 import re
 import json
+from git import Repo
 from utils.timeutils import TimeUtils
-from utils.request import BitbucketRequests
+from utils.requestutils import BitbucketRequests
 
 class Commits:
 
-    URL = ""
+    def __init__(self, repo):
+        self.repo = Repo(repo)
 
-    def __init__(self, username, password):
-        self.reqs = BitbucketRequests(username, password)
+    def commits(self, since=""):
+        i = 1   # skip
+        since = TimeUtils().past(days=20)
+        commit = self._get_one()
+        while (commit is not None) and (commit.committed_datetime > since):
+            print str(commit.author) + " : " + str(commit.message)
+            commit = self._get_one(skip=i)
+            i = i + 1
 
-    def all(self, pagelen=5, page=1):
-        ''' returns all commits '''
-        url = self.URL + "?page=" + str(page)
-        url = url + "&pagelen=" + str(pagelen)
-        return self.reqs.get(url).text
+    def _get_one(self, skip=0):
+        ''' returns single commit using skip '''
+        commits = list(self.repo.iter_commits(max_count=1, skip=skip))
+        if commits:
+            return commits[0]
+        return None
 
+    def _info(self, commit):
+        ''' returns a dictionary filled with information about particular commit '''
+        info = {}
+        pass
---
+++ res/user.py
@@ -0,0 +1,10 @@
+
+
+class Users:
+    """ User Statistics """
+    def __init__(self):
+        '''  '''
+        self.users = {}
+
+    def _add_new(self, prop):
+        pass
