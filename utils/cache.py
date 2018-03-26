41df75d011cb664c7f3afc4deb08f2e22de90be0
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2018-03-26 20:26:13 +0530

    added stub for cache helpers

---
+++ utils/cache.py
@@ -0,0 +1,26 @@
+class Cache(object):
+    """ generic object cache """
+
+    def __init__(self):
+        pass
+
+    def put(self):
+        ''' puts new document in cache '''
+        pass
+
+    def get(self):
+        ''' returns a document from cache '''
+        pass
+
+
+
+class CommitCache(Cache):
+    """ commits cache """
+
+    def __init__(self):
+        ''' '''
+        pass
+
+    def put(self):
+        ''' '''
+        pass
